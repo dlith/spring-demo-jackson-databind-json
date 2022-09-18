267c61526aa9f0f95f42858c48ca09601cc08a1d
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-18 14:26:23 +0200

    ignore properties from json

--- src/main/java/com/dzmitry/Student.java
+++ src/main/java/com/dzmitry/Student.java
@@ -1,10 +1,12 @@
 package com.dzmitry;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 
     private int id;
